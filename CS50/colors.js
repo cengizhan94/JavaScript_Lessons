b305a6352e8d263b83182459adbf2e2ac0077c22
cengizhan94 <sonsair11@gmail.com>
2023-07-14 16:19:25 +0300

    update color changing

--- CS50/colors.js
+++ CS50/colors.js
@@ -1,15 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Change font color to red
-    document.querySelector('#red').onclick = function(){
-        document.querySelector('#colors').style.color = 'red';
-    }
-    //Change font color to blue
-    document.querySelector('#blue').onclick = function(){
-        document.querySelector('#colors').style.color = 'blue';
-    }
-    //Change font color to green
-    document.querySelector('#green').onclick = function(){
-        document.querySelector('#colors').style.color = 'green';
-    }
-
+    document.querySelectorAll('button').forEach(function(button){
+        button.onclick = function(){
+            document.querySelector('#colors').style.color = button.dataset.color;
+        }
+    });
 });
