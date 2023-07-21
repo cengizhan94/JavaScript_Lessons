b42fd85721f850b6a1eeab05c15cb105ab7cc0ec
cengizhan94 <sonsair11@gmail.com>
2023-07-21 14:16:40 +0300

    Tasks updated

---
+++ CS50/task.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    //By default, submit button is disabled
+
+    document.querySelector('#submit').disabled = true;
+
+    document.querySelector('#task').onkeyup = () =>{
+
+        /*if(document.querySelector('#task').value.lengh > 0){
+            document.querySelector('#submit').disabled = false;
+        }else{
+            document.querySelector('#submit').disabled = true;
+        }*/
+        
+        document.querySelector('#submit').disabled = false;
+        
+    }
+
+    document.querySelector('form').onsubmit = () =>{
+      const task = document.querySelector('#task').value;
+      
+      const li = document.createElement('li')
+      li.innerHTML = task;
+
+      document.querySelector('#tasks').append(li);
+
+      document.querySelector('#task').value = '';
+      document.querySelector('#submit').disabled = true;
+
+      return false;
+    }
+});
