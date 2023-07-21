cd7217241c1c0d8d14161d213840959a8a0db87a
cengizhan94 <sonsair11@gmail.com>
2023-07-21 11:51:50 +0300

    arrow notation

--- CS50/colors.js
+++ CS50/colors.js
@@ -1,7 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('button').forEach(function(button){
+document.addEventListener('DOMContentLoaded', () => {
+    /*document.querySelectorAll('button').forEach(  button =>{
         button.onclick = function(){
             document.querySelector('#colors').style.color = button.dataset.color;
         }
-    });
+    });*/
+
+    document.querySelector('select').onchange = function() {
+        document.querySelector('#colors').style.color = this.value;
+    }
 });
