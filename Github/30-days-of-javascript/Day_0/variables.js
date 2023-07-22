e05a34a52d8756afdddb6cedd2cfaa707b385bef
cengizhan94 <sonsair11@gmail.com>
2023-07-22 18:16:56 +0300

    variables

---
+++ Github/30-days-of-javascript/Day_0/variables.js
@@ -0,0 +1,12 @@
+let firstName = 'Cengiz Han'
+let lastName = 'Uyar'
+let country = 'Turkey'
+let city ='Tokat'
+let age ='30'
+let isMarried = false
+
+const gravity = 9.81
+const boilingPoint = 100 
+const PI = 3.14
+
+let name = 'Cengiz Han', job = 'Student', live = 'Turkey'
