15efa54be984a4d8fa4879ae4560ec60e38057fc
cengizhan94 <sonsair11@gmail.com>
2023-07-23 13:40:45 +0300

    primative data types

--- Github/30-days-of-javascript/Day_1/string_methods/non_primative_data_types.js
+++ Github/30-days-of-javascript/Day_1/string_methods/non_primative_data_types.js
@@ -1,3 +1,35 @@
 let nums = [1,2,3]
 nums[0] = 10
 console.log(nums)
+console.log(typeof(nums))
+
+nums = [1,2,3]
+let numbers= [1,2,3]
+console.log(nums == numbers)
+
+
+let userOne={
+    name : 'Cengiz Han',
+    role : 'student',
+    country : 'Turkey'
+}
+console.log(typeof(userOne))
+
+let userTwo = {
+    name: "Zeynep",
+    role : "Student",
+    country: "Turkey"
+}
+console.log(userOne == userTwo)
+
+ numbers = nums
+ console.log(nums == numbers)
+
+ userOne = {
+    name : "Cengiz Han",
+    role : "Student",
+    country : "Turkey"
+ }
+userTwo = userOne
+ console.log(userOne == userTwo)
+
