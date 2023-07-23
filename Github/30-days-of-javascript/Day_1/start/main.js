783d54978d65e278c1a00573cd7d03289f4befa6
cengizhan94 <sonsair11@gmail.com>
2023-07-23 13:33:12 +0300

    Day2 math objects and primative data types.

---
+++ Github/30-days-of-javascript/Day_1/start/main.js
@@ -0,0 +1,29 @@
+/*let challenge = '30 Days Of JavaScript';
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(2,21))
+console.log(challenge.substring(0,2))
+console.log(challenge.includes("Script"))
+console.log(challenge.split(" "))
+let str = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(str.split(","))
+console.log(challenge.replace("JavaScript", "Python"))
+console.log(challenge.charAt([15]))
+console.log(challenge.charCodeAt("J"))
+console.log(challenge.indexOf("a"))
+console.log(challenge.lastIndexOf("a"))
+
+let str2 = "You can't end a sentence with because because because was a conjunction."
+console.log(str2.indexOf("because"))
+console.log(str2.lastIndexOf("beause"))
+console.log(challenge.trim())
+console.log(challenge.startsWith("3"))
+console.log(challenge.endsWith("t"))
+console.log(challenge.match("a"))
+console.log(challenge.concat("30 Days Of", "JavaScript"))
+console.log(challenge.repeat(2))
+*/
+
+
