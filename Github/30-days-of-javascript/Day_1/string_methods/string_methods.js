415c314614e56ae07914c34e6b170844c2a6c2ec
cengizhan94 <sonsair11@gmail.com>
2023-07-25 11:06:08 +0300

    Exercise

---
+++ Github/30-days-of-javascript/Day_1/string_methods/string_methods.js
@@ -0,0 +1,39 @@
+//accessin_characters
+
+/* let string = "JavaScript"
+let firstLetter = string[0]
+console.log(firstLetter);
+
+let secondLetter = string[1]
+let thirdLetter = string[2]
+let lastLetter = string[9]
+
+console.log(lastLetter);
+
+let lastIndex = string.length - 1
+
+console.log(lastIndex);
+console.log(string[lastIndex]) */
+
+//concat
+
+/* let string = "30"
+console.log(string.concat("Days", "of", "JavaScript"))
+let country = "TR"
+console.log(country.concat("land")) */
+
+//ends_with
+
+/* let string = "Love is the best to in this world"
+console.log(string.endsWith("world"));
+console.log(string.endsWith("Love"));
+console.log(string.endsWith("in this world"))
+
+let country = "Turkey"
+
+console.log(country.endsWith("land"))
+console.log(country.endsWith("tur"))
+console.log(country.endsWith("Tur")) */
+
+//includes
+
