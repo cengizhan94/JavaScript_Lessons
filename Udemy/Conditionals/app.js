8b2e1e1d987bbb28a39c7b65564268be431164c6
cengizhan94 <sonsair11@gmail.com>
2023-07-26 06:28:11 +0300

    JavaScript Conditionals

---
+++ Udemy/Conditionals/app.js
@@ -0,0 +1,11 @@
+//if else Ternary Operator
+
+const number = 120;
+
+/* if (number === 100){
+    console.log("Number is 100")
+}else{
+    console.log("Number is not 100")
+} */
+
+console.log(number === 100 ? "Number is 100" : "Number is not 100")
