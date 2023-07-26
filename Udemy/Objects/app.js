c28cc1cf8a7678f6683228cc7b3c3ac8549b9c3c
cengizhan94 <sonsair11@gmail.com>
2023-07-26 06:19:50 +0300

    Date Object Examples

--- Udemy/Objects/app.js
+++ Udemy/Objects/app.js
@@ -1,6 +1,6 @@
 let value;
 
-const programmer  = {
+/* const programmer  = {
     name : "Cengiz Han Uyar",
     age : 25,
     email: "sonsair11@gmail.com",
@@ -23,5 +23,25 @@ const programmers = [
     {name : "Cengiz Han", age: 30},
     {name: "Zeynep", age: 12},
 ]
-value = programmers[0].name;
+value = programmers[0].name; */
+
+//Date object
+
+const now = new Date();
+const birthdate = new Date("04-25-1994 07:00:00")
+const birthdate2 = new Date("April 25 1994")
+const birthdate3 = new Date ("04/25/1994")
+value = birthdate;
+
+value = birthdate.getMonth();//January is 0 not 1
+//value = birthdate.getDate();
+//value = birthdate.getDay();
+value = birthdate.getHours()
+value = birthdate.getMinutes()
+value = birthdate.getSeconds()
+value = birthdate.getMilliseconds()
+
+
+
 console.log(value)
+
