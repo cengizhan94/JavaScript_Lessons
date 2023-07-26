ef652a1b67e46757fe17a535e1c605149441013d
cengizhan94 <sonsair11@gmail.com>
2023-07-26 14:54:29 +0300

    window object and methods

---
+++ Udemy/WindowObject/app.js
@@ -0,0 +1,39 @@
+ 
+ //confirm
+
+/*  if (confirm("Delete?")){
+    console.log("deleted.")
+ }else{
+    console.log("not delete")
+ }
+ */
+
+ //prompt
+
+/*  const answer = prompt("2+2 = ?");
+
+ if (answer == "4"){
+    console.log("Correct!")
+ }else{
+    console.log("Wrong!");
+ } */
+
+ let value;
+value = window;
+ value = window.location;
+ value = window.location.host;
+
+ value = window.outerHeight;
+ value= window.outerWidth;
+
+ value = window.innerHeight;
+ value = window.outerWidth;
+ 
+ console.log(value);
+/*  
+ if (confirm("Reload the page?")){
+    window.location.reload();
+ }else{
+    console.log("page is not reloaded.")
+ } */
+
