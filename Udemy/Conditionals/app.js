3f2eaef2640d1eb8f2d8f61e0eea557a5ec5529f
cengizhan94 <sonsair11@gmail.com>
2023-07-26 06:41:48 +0300

    conditionals if-else , swich-case

--- Udemy/Conditionals/app.js
+++ Udemy/Conditionals/app.js
@@ -9,3 +9,55 @@ const number = 120;
 } */
 
 console.log(number === 100 ? "Number is 100" : "Number is not 100")
+
+if (number === 100) console.log("Number is 100")
+
+else console.log("Number is not 100")
+
+
+//Swich - Case
+
+const process = 1;
+
+switch(process){
+    case 1: 
+        console.log("Process 1");
+        break;
+    case 2:
+        console.log("process 2");
+        break;
+    case 3:
+        console.log("process 3");
+        break;
+    default:
+        console.log("undefined process");
+
+}
+
+switch (new Date().getDay()){
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    case 7:
+        day = "Sunday";
+}
+
+document.getElementById("demo").innerHTML = "Today is " + day;
