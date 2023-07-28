9e3dd4ee97be162a541061c3a44b201a41f77d3a
cengizhan94 <sonsair11@gmail.com>
2023-07-28 17:27:20 +0300

    challenge2 exercises calculating math problems

---
+++ Github/30-days-of-javascript/Day_2/Challenges/challenge2.js
@@ -0,0 +1,44 @@
+//Exercise Level2
+//1-Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
+/* const base = parseFloat(prompt("Enter the base length of the triangle: "))
+const height = parseFloat(prompt("Enter the height of the triangle: "))
+
+const area = 0.5 * base * height;
+
+console.log("Area of triangle: ", area) */
+
+//2- Write a script that prompt the user side a, side b, and side c of the  triangle and calculate the perimeter of triangle (perimeter = a + b + c)
+
+/* const sidea = parseFloat(prompt("Enter the side a of triangle"))
+const sideb = parseFloat(prompt("Enter the side b of triangle"))
+const sidec = parseFloat(prompt("Enter the side c of triangle"))
+
+const perimeter = sidea + sideb + sidec;
+console.log("Perimeter of triangle: ", perimeter) */
+
+// -3 Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+
+/* const length = parseFloat(prompt("Enter the length of rectangle"))
+const width = parseFloat(prompt("Enter the width of rectangle"))
+const perimeter = 2 * length + width;
+console.log("Perimeter of rectangle" , perimeter) */
+
+//4- Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+/* const radius = parseFloat(prompt("Ener the radius of circle"))
+const pi = Math.PI;
+const area = pi * radius * radius
+const circrumference = 2 * pi * radius;
+console.log("Circle area ",area)
+console.log("Circumference ",circrumference) */
+
+//5- Calculate the slope, x-intercept and y-intercept of y = 2x-2
+/* const coefficientA = parseFloat(prompt("Enter the coefficient of the equation x"))
+const coefficientB = parseFloat(prompt("Enter the constant term of equation"))
+const slope = coefficientA
+const xIntercept = -coefficientB / coefficientA;
+const yIntercept = coefficientB
+console.log("Slope ", slope)
+console.log("xIntercept ",xIntercept)
+console.log("yIntercept", yIntercept) */
