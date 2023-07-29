56d38d8cd81ab52e2a07af2e3b378bba4f29ccd4
cengizhan94 <sonsair11@gmail.com>
2023-07-29 12:27:52 +0300

    problem 4 solved

--- Github/30-days-of-javascript/Day_3/Challenges/challenge_1.js
+++ Github/30-days-of-javascript/Day_3/Challenges/challenge_1.js
@@ -51,3 +51,12 @@ const b = 14; */
     ?`${b} greater than ${a}`:
     `${a} equals to ${b}`;
 console.log(result) */
+
+//4- Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+const number = parseFloat(prompt("Enter a number:"))
+
+if (number % 2 === 0){
+    console.log(`${number} is an even number.`)
+}else{
+    console.log(`${number} is an odd number.`)
+}
