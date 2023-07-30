2f0f7fdfd0b14622a4e0dced5862ebe20c027a74
cengizhan94 <sonsair11@gmail.com>
2023-07-30 10:38:52 +0300

    Day 5 arrays

---
+++ Github/30-days-of-javascript/Day_4/Lessons/app.js
@@ -0,0 +1,70 @@
+/* //Arrays
+//Using Array constructor
+const arr = Array();
+//let arr = new Array();
+console.log(arr) //[]
+
+//Using square brackets([])
+const arr2 = []
+console.log(arr2) //[]
+ */
+
+//Array values
+/* const numbers = [0,3.14,9.81,23,37,98.6,100]
+const fruits = ['apple','banan','cherry', 'mango','lemon','orange']
+const vegatables = ['Tomato','Patato','Cabbage','Onion','Carrot']
+const animalProducts =['Milk','Meat','Butter','Yogurt']
+const webTechs = ['HTML','CSS','JS','React','Redux','Noe','MongoDB']
+const countries = ['Turkey']
+
+console.log('Numbers:',numbers)
+console.log('Number of numbers: ', numbers.length)
+
+console.log("Fruits: ", fruits)
+console.log("Number of fruits: ",fruits.length) */
+
+//Array with different types.
+/* const arr = [
+    'Cengiz Han',
+    29,
+    false,
+    {country: 'Turkey', city: 'Tokat'},
+    {skills: ['HTML','CSS','JS','Django','Python']}
+]
+console.log(arr) */
+
+//Array split
+
+/* let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript) */
+
+//Accessing array items using index
+/* 
+const names = ['Cengiz Han','Oguz','Zafer','Zeynep','Pervin','Mustafa']
+let firstName = names[0]
+console.log(firstName)
+
+let lastIndex = names.length-1
+lastName = names[lastIndex]
+console.log(lastName) */
+
+//Modifying array element
+/* const numbers = [1,2,3,4,5,6]
+numbers[0] = 10
+numbers[1] = 20
+console.log(numbers) */
+
+//Methods to manipulate array
+/* const arr = Array()
+console.log(arr)
+
+const eightEmptyValues = Array(8)
+console.log(eightEmptyValues) */
+
+//Creating static values with fill
+
+
+
+
