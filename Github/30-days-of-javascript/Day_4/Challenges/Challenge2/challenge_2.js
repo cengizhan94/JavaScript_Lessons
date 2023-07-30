21d72e0d763ad4e96a679cc733807983cca9bfd5
cengizhan94 <sonsair11@gmail.com>
2023-07-30 17:49:00 +0300

    challenge level 2 started

---
+++ Github/30-days-of-javascript/Day_4/Challenges/Challenge2/challenge_2.js
@@ -0,0 +1,7 @@
+//challenge level 2
+//1- Create separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file.
+import countries from './countries.js'
+import webTechs from './web_techs.js'
+
+console.log("Countries: ", countries);
+console.log("Web Techs: ", webTechs);
