ee3086dbf4281d70a0cebcc71c464d7467ef9619
cengizhan94 <sonsair11@gmail.com>
2023-08-02 19:16:54 +0300

    challenge 2 problem 7-8 solved

--- Github/30-days-of-javascript/Day_5/Challenges/challenge_2.js
+++ Github/30-days-of-javascript/Day_5/Challenges/challenge_2.js
@@ -2,53 +2,53 @@
 //1- Develop a small script with generate any number of characters random id
 
 const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-let randomID = "";
+/* let randomID = "";
 
 for(let i = 0; i < 32; i++){
     const randomIndex = Math.floor(Math.random()* charset.length);
     randomID += charset[randomIndex];
 }
-console.log("Random ID: ", randomID)
+console.log("Random ID: ", randomID) */
 //2-Write a script which generates a random hexadecimal number:
-let colorCode = "#";
+/* let colorCode = "#";
 const hexadecimalChars = "0123456789ABCDEF";
 for(let i = 0; i < 6; i++){
     const randomIndex = Math.floor(Math.random()*hexadecimalChars.length);
     colorCode += hexadecimalChars[randomIndex];
 }
-console.log("Random hexadecimal code: ",colorCode)
+console.log("Random hexadecimal code: ",colorCode) */
 
 //3- Write a script which generates a random rgb color numer.
-const red = Math.floor(Math.random()*256);
+/* const red = Math.floor(Math.random()*256);
 const green = Math.floor(Math.random()*256);
 const blue = Math.floor(Math.random()*256);
 
 const randomRGBCollor = `rgb(${red}, ${green},${blue})`;
-console.log("Random RGB color: ",randomRGBCollor)
+console.log("Random RGB color: ",randomRGBCollor) */
 
 //4- Using the below countries array, create the following new array.
 let countries = ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"] 
-let randomCountries = []
+/* let randomCountries = []
 
 for(let i = 0; i < countries.length; i++){
     const randomCountry = Math.floor(Math.random()*countries.length)
     randomCountries.push(countries[randomCountry])
 }
-console.log(randomCountries)
+console.log(randomCountries) */
 
 //5-Using the above countries array, create an array for countries length.
 
 
-for (let i = 0; i < countries.length; i++) {
+/* for (let i = 0; i < countries.length; i++) {
   const randomCountryIndex = Math.floor(Math.random() * countries.length);
   const randomCountry = countries[randomCountryIndex];
   randomCountries.push(randomCountry);
 
   console.log(`${randomCountry}'s length:`, randomCountry.length);
-}
+} */
 //6->
 
-const result = []
+/* const result = []
 for(let i = 0; i < countries.length; i++){
     const country = countries[i]
     const abbreviation = country.substring(0,3).toUpperCase();
@@ -58,7 +58,40 @@ for(let i = 0; i < countries.length; i++){
     result.push(countryData);
 }
 
-console.log(result)
+console.log(result) */
 
 //7- In above countries array, check if there is a country or countries containing the word'land'. If there are countries containing 'land', print it as array. If there is no country containing the word 'land', print'All these countries are without land'
+let countriesWithLand = []
+let hasCountryWithLand = false;
+
+for(let i = 0; i < countries.length; i++){
+    if(countries[i].toLowerCase().includes('land')){
+        countriesWithLand.push([countries[i]]);
+        hasCountryWithLand = true;
+    }
+}
+
+if (hasCountryWithLand){
+    console.log(countriesWithLand);
+}else{
+    console.log("All these countries are without land")
+}
+//8- In above countries array, check if there is a country or countries end with a substring 'ia'. If there are countries end with, print it as array. If there is no country containing the word 'ai', print 'These are countries ends without ia'.
+
+let countriesWithia = [];
+let hasCountryWithia = false;
+
+for(let i = 0; i < countries.length; i++){
+    if(countries[i].toLowerCase().includes('ia')){
+        countriesWithia.push([countries[i]])
+        hasCountryWithia = true;
+    }
+}
+if(hasCountryWithia){
+    console.log(countriesWithia);
+}else{
+    console.log("These are countries ends without ia")
+}
+
+// 9- Using the above countries array, find the country containing the biggest number of characters.
 
